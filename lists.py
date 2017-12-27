0c52b83c698af576e3891443ea97b9c04441788a
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-26 19:59:57 -0500

    Intial learning of lists

---
+++ lists.py
@@ -0,0 +1,19 @@
+bikes = ['trek','redline','giant']
+
+first_bike = bikes[0]
+
+last_bike = bikes[-1]
+
+for bike in bikes:
+    print (bike)
+
+bikes.append('aravind')
+bikes.append('kumar')
+
+
+squares = []
+for i in range(1,11):
+    squares.append(i**2)
+
+for num in squares :
+    print(num)
