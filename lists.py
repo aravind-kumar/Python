b97d77b603dd2c40e3c9fee4d0e4c8ee3bae3a03
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-26 20:07:36 -0500

    Completed lists

--- lists.py
+++ lists.py
@@ -11,9 +11,29 @@ bikes.append('aravind')
 bikes.append('kumar')
 
 
+print "The following is range based for loop where the first one is the start and the second is end range"
 squares = []
 for i in range(1,11):
     squares.append(i**2)
 
 for num in squares :
     print(num)
+
+
+print "The following is using list comprehesion the first part is the body followed by the for loop"
+squarescompre = [x**2 for x in range(1,11)]
+
+for num in squarescompre:
+    print num
+
+print "The following is list splicing where the first num is the start and the last one is the end range"
+first_half = squarescompre[:6]
+second_half = squarescompre[6:]
+
+print "First"
+for num in first_half:
+    print num
+
+print "Second"
+for num in second_half:
+    print num
