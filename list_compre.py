23b98dde7c3aeda3eaf3defc9252a906bbe9b72f
aravind-kumar <karavindkumar1993@gmail.com>
2018-01-02 22:49:08 -0500

    Complteted third exercise

--- list_compre.py
+++ list_compre.py
@@ -1,9 +1,17 @@
 nums = [num for num in range(1,1000) if num%7==0]
-print nums 
+print (nums) 
 
 nums1 = [num for num in range(0,1000,7)]
-nums1 = nums[0:]
-print nums1
+nums1 = nums1[0:]
+print (nums1)
 
 numswith3 = [num for num in range(1,1000) if '3' in str(num)]
-print numswith3
+print (numswith3)
+
+text="My name is aravind kumar"
+print (len(text.split())-1)
+
+vowels=['a','e','i','o','u']
+
+withoutvowels = [char for char in text if char not in vowels]
+print "".join(withoutvowels)
